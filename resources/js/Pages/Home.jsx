670250444dc69fa2740dd475a344c66f1a831327
Becca Fiorillo <estevaovrb2014@gmail.com>
2025-08-02 12:24:29 -0300

    HELLO WORLD RODOU VAI TOMANDO

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+// resources/js/Pages/Home.jsx
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div className="p-6">
+            <h1 className="text-3xl font-bold">Bem-vindo!</h1>
+            <p>Esta é a Home renderizada via Inertia e React.</p>
+        </div>
+    );
+}
