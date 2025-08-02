a41bad61967944a02cb5208c4c6f9fa43435aa28
Leandro Carlos <melo.leandro@estudante.ufjf.br>
2025-08-02 20:12:15 -0300

    rota do ponto de venda

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/pointOfSale', function () {
+    return Inertia::render('PointOfSale');
+})->middleware(['auth', 'verified'])->name('pointOfSale');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
