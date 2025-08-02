957539e26317175118bd3487db398207b7cf0ccb
Leandro Carlos <melo.leandro@estudante.ufjf.br>
2025-08-02 20:28:27 -0300

    to cansado nmrl

--- resources/js/Pages/PointOfSale.jsx
+++ resources/js/Pages/PointOfSale.jsx
@@ -7,9 +7,12 @@ export default function PointOfSale() {
                 <div className="text-5xl font-bold text-center mt-10 bg-red-500 text-white pt-5 pb-5 rounded-lg">
                     <h1>temp</h1>
                 </div>
-                <div class="flex">
-                    <div className="w-1/2 bg-red-200">Column 1</div>
-                    <div className="w-1/2 bg-green-200">Column 2</div>
+                <div className="flex">
+                    {/* put the text at the bottom of the div */}
+                    <div>
+                        <div className="self-end">Column 1</div>
+                    </div>
+                    <div className="flex w-1/2 h-96 bg-green-200 text-5xl font-semibold items-center justify-center">Column 2</div>
                 </div>
             </div>  
         </AuthenticatedLayout>
