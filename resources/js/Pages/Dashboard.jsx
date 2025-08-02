28edefe9b8e8e2b81bbd860c8395b8a1f63a21e6
Becca Fiorillo <estevaovrb2014@gmail.com>
2025-08-02 19:31:21 -0300

    fodaq certas coisa

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,24 +1,40 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import { useEffect } from 'react';
 
 export default function Dashboard() {
+    
+
     return (
         <AuthenticatedLayout
             header={
                 <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Dashboard
+                    PONK - Point of Sale System
                 </h2>
             }
         >
-            <Head title="Dashboard" />
-
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            You're logged in!
-                        </div>
+            <Head title="Ponk" />
+            <div className="main-container">
+                <div
+                    className="menu-card"
+                    onClick={() => handleMenuClick('pos')}
+                >
+                    <div className="menu-card-content">
+                        <div className="menu-title">PONTO DE VENDA</div>
+                        <div className="menu-icon">🛍️</div>
                     </div>
+                    <div className="menu-key">F1</div>
+                </div>
+
+                <div
+                    className="menu-card"
+                    onClick={() => handleMenuClick('status')}
+                >
+                    <div className="menu-card-content">
+                        <div className="menu-title">STATUS DO CAIXA</div>
+                        <div className="menu-icon">📈</div>
+                    </div>
+                    <div className="menu-key">F2</div>
                 </div>
             </div>
         </AuthenticatedLayout>
