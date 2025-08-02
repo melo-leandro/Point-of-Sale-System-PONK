423b31f571843d32184882c8dbab9bccf568e36d
Becca Fiorillo <estevaovrb2014@gmail.com>
2025-08-02 16:45:31 -0300

    O LOGIN FUNCIONA!!!

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -21,6 +21,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
 });
 
 require __DIR__.'/auth.php';
