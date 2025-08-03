4e584d629d63c1611042c42400e830612cbf3b2f
Cauã Moreno <cauam7344@gmail.com>
2025-08-02 22:50:58 -0300

    conserta migrations

--- database/migrations/2025_08_02_100000_create_usuarios_table.php
+++ database/migrations/2025_08_02_100000_create_usuarios_table.php
@@ -12,7 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('usuarios', function (Blueprint $table) {
+<<<<<<< HEAD
             $table->char('cpf', 11)->comment('CPF sem formatação')->primary();
+=======
+            $table->string('cpf', 11)->comment('CPF sem formatação')->primary();
+>>>>>>> 8941c51 (conserta migrations)
             $table->timestamps();
             $table->string('nome');
             $table->string('email')->unique();
@@ -30,20 +34,28 @@ return new class extends Migration
             CHECK (pin IS NULL OR (pin ~ '^[0-9]{4}$' AND LENGTH(pin) = 4));"
         );
 
+<<<<<<< HEAD
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->timestamps();
             $table->string('token');
         });
+=======
+        // Schema::create('senha_reset_tokens', function (Blueprint $table) {
+        //     $table->string('email')->primary();
+        //     $table->timestamps();
+        //     $table->string('token');
+        // });
+>>>>>>> 8941c51 (conserta migrations)
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
+        // Schema::create('sessions', function (Blueprint $table) {
+        //     $table->string('id')->primary();
+        //     $table->foreignId('user_id')->nullable()->index();
+        //     $table->string('ip_address', 45)->nullable();
+        //     $table->text('user_agent')->nullable();
+        //     $table->longText('payload');
+        //     $table->integer('last_activity')->index();
+        // });
     }
 
     /**
@@ -52,7 +64,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('usuarios');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        // Schema::dropIfExists('password_reset_tokens');
+        // Schema::dropIfExists('sessions');
     }
 };
