9c12d9b1fe03c5695a8e8306cdd89e683961b3d1
Becca Fiorillo <estevaovrb2014@gmail.com>
2025-08-03 00:33:47 -0300

    agora so falta fazer a lista de itens

--- resources/js/Pages/StatusCaixa.jsx
+++ resources/js/Pages/StatusCaixa.jsx
@@ -37,15 +37,17 @@ export default function StatusCaixa() {
                 <div className="w-full max-w-[1200px] h-[80vh] px-6">
                     <div id="item-panel" className="flex gap-4">
                         <div className="2/5 flex w-1/3 flex-col gap-4">
-                            <div className="flex h-32 flex-col bg-red-200">
-                                <div className="self-start pb-3 pl-7 pt-2 text-3xl font-semibold">
-                                    Desconto
+                            <div className="flex h-32 flex-col items-center justify-center bg-green-200">
+                                <div className="text-1xl flex items-start justify-center font-semibold">
+                                    STATUS DO CAIXA
                                 </div>
-                                <div className="flex justify-center pb-2">
-                                    <input
-                                        className="h-12 w-[300px] resize-none overflow-hidden border px-3 text-xl font-semibold"
-                                        placeholder="Insira o valor desejado..."
-                                    />
+                                <div className="flex justify-center">
+                                    <h2 className="text-5xl font-semibold">
+                                        ABERTO
+                                    </h2>
+                                </div>
+                                <div className="text-1xl flex items-start justify-center font-semibold">
+                                    abertura e data eu acho texto grande
                                 </div>
                             </div>
 
