3af41f8363cc8918937bc0aa46e6f83c68282891
Bebs Fiorillo <estevaovrb2014@gmail.com>
2025-08-05 21:33:18 -0300

    nao entendo mais

--- resources/js/Components/ConfirmarCancelamentoPopUp.jsx
+++ resources/js/Components/ConfirmarCancelamentoPopUp.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../../css/confirmarCancelamentoPopUp.css';
+import '../../css/confirmarCancelamentoPopUp.css';
 
 const ConfirmarCancelamentoPopUp = ({ 
     aparecendo, 
